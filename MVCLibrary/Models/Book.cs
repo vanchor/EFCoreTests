0540e229d1e53bd96526cd40896f637ccd8948f9
vanchor <vovanyakorev2@gmail.com>
2022-08-27 18:03:31 +0200

    Added Book model

---
+++ MVCLibrary/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCLibrary.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; }
+    }
+}
